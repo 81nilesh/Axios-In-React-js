887fd0e68005e3dda9671c7a7d35905f56b01614
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-10-28 20:49:08 +0530

    get Data

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,30 @@ import axios from "axios";
 
 function App() {
   const [myData, setMyData] = useState([]);
+  const [isError, setIsError] = useState("");
 
   useEffect(() => {
     axios
       .get("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => setMyData(res.data));
+      .then((res) => setMyData(res.data))
+      .catch((error) => setIsError(error.message));
   }, []);
 
   return (
     <>
       <h1>Axios Tutorial</h1>
-      {myData.map((post) => {
-        const { id, title, body } = post;
-        return (
-          <div className="card" key={id}>
-            <h2>{title}</h2>
-            <p>{body}</p>
-          </div>
-        );
-      })}
+      {isError !== "" && <h2>{isError}</h2>}
+      <div className="grid">
+        {myData.map((post) => {
+          const { id, title, body } = post;
+          return (
+            <div className="card" key={id}>
+              <h2>{title.slice(0, 15).toUpperCase()}</h2>
+              <p>{body.slice(0, 100)}</p>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
